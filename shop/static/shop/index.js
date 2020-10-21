6ca9e4b43c0e3202da22da27230607ce8c4330b3
RifatRobin <rifatitna@gmail.com>
2020-10-22 01:22:53 +0600

    added js script as a .js file in static folder, created cart link for navbar, added logic for the cart to show how many products are selected not the quantity of each product then kept it in localStorage

---
+++ shop/static/shop/index.js
@@ -0,0 +1,29 @@
+console.log('statrted working');
+$(".card-text").hide();
+$(".details").click(function () {
+    $(".card-text").show(2000);
+});
+
+
+if (localStorage.getItem('cart') == null) {
+    var cart = {};
+}
+else {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    document.getElementById('crt').innerHTML = Object.keys(cart).length;
+}
+
+$('.cart').click(function () {
+    var prd_id = this.id.toString();
+    //console.log("you clicked on " + prd_id);
+    if (cart[prd_id] == null) {
+        cart[prd_id] = 1;
+    }
+    else {
+        cart[prd_id] = cart[prd_id] + 1;
+    }//this will allow to see how many cart you clicked & it will endup with session reload
+    console.log(cart);
+
+    //now lets update the cart as it clicked
+    localStorage.setItem("cart", JSON.stringify(cart));
+});
